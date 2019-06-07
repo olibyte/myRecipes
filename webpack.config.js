c03d26f68b53d35d567e2c686ea36c2750792f12
Oliver <ocben1@gmail.com>
2019-06-07 11:36:32 +1000

    Mount root component

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
+const path = require('path');
 const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    context: path.join(__dirname, 'src'),
     entry: './index.js',
     output: {
-        path: __dirname,
-        filename: './bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: '.bundle.js'
     },
     module: {
         rules: [
@@ -13,5 +16,11 @@ module.exports = {
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: 'body'
+        })
+    ]
 };
